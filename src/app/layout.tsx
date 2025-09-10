43022b6d8b5ff877cab320f53e9da77ed15945c6
Wagner Lopes <jlsolucoesivp1@gmail.com>
2025-09-10 14:32:51 +0000

    nao esta alterando o tema

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
+          themes={['light', 'dark', 'stone', 'orange', 'rose', 'green', 'zinc']}
           disableTransitionOnChange
         >
           {children}

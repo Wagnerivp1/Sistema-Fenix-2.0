33101c572478fe0cf074033814e95379d0c48b3d
Wagner Lopes <jlsolucoesivp1@gmail.com>
2025-09-24 12:00:40 +0000

    ⚠ Cross origin request detected from 192.168.100.205 to /_next/* resourc

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    allowedDevOrigins: [
+      'http://localhost:3000',
+      'http://192.168.100.205',
+    ]
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
